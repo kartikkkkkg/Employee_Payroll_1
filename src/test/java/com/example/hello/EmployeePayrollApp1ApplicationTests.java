e2c21ad5860a440e8069605521a6e960685aaba0
kartikkkkg <kartikgupta.jaipur@gmail.com>
2025-05-13 10:00:00 +0530

    Added UC1 – REST API with GET, POST, PUT, DELETE

---
+++ src/test/java/com/example/hello/EmployeePayrollApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
